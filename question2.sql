0899acf6739adbcc6dc0b06c3d9495529caf8424
som2023 <josephsom44@gmail.com>
2023-02-16 13:27:37 +0000

    First commit

---
+++ question2.sql
@@ -0,0 +1,2 @@
+INSERT INTO actors(firt_name,last_name,age,number_oscars) VALUES(' ',' ',' ','');
+-- resultat: impossible parce que nous avons dit que les valeurs des champs ne doivent pas etre null; 
